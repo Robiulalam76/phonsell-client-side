4a1f77ab5a7b051d35cbd61566d5c587dccc4a17
robiulalam76 <robiulalam762400@gmail.com>
2022-11-24 03:20:00 +0600

    singup page add

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,12 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Home from "../../Pages/Home/Home";
+import Login from "../../Pages/Profile/Login/Login";
+import Signup from "../../Pages/Profile/Signup/Signup";
 
 const router = createBrowserRouter([
     {
         path: '/', element: <Main></Main>, children: [
             { path: '/', element: <Home></Home> },
             { path: '/home', element: <Home></Home> },
+            { path: '/login', element: <Login></Login> },
+            { path: '/signup', element: <Signup></Signup> },
         ]
     }
 ])
