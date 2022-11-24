02d9013926db41228c4c247ee67677db08b96524
robiulalam76 <robiulalam762400@gmail.com>
2022-11-24 22:43:44 +0600

    wishlist system added

--- src/Pages/Products/ProductCard/ProductCard.js
+++ src/Pages/Products/ProductCard/ProductCard.js
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ProductCard = ({ product }) => {
     const { _id, categoryId, brand, name, image, price, location, sold, condition, used, orginalPrice, Model, authenticity, features, description, } = product;
@@ -14,10 +14,10 @@ const ProductCard = ({ product }) => {
         }
     })
 
-
     const wishlised = wishlist.find(w => w.serviceId === product._id)
 
-    const handleWishlist = (id) => {
+
+    const handleWishlist = () => {
         const wishlist = {
             serviceId: _id,
             categoryId,
@@ -36,21 +36,9 @@ const ProductCard = ({ product }) => {
             description,
         }
 
-        if (id === wishlist.serviceId) {
-            fetch(`http://localhost:5000/wishlist/${wishlist.serviceId}`, {
-                method: 'DELETE',
-                headers: {
-                    'content-type': 'application/json'
-                },
-            })
-                .then(res => res.json())
-                .then(data => {
 
-                    console.log(data);
-                    refetch()
 
-                })
-        }
+
         fetch('http://localhost:5000/wishlist', {
             method: 'POST',
             headers: {
@@ -61,6 +49,20 @@ const ProductCard = ({ product }) => {
             .then(res => res.json())
             .then(data => {
                 if (data.acknowledged) {
+                    // console.log(data);
+                    refetch()
+                }
+            })
+
+    }
+
+    const handleRemoveWishlist = (id) => {
+        fetch(`http://localhost:5000/wishlist/${id}`, {
+            method: 'DELETE',
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
                     console.log(data);
                     refetch()
                 }
@@ -91,13 +93,13 @@ const ProductCard = ({ product }) => {
                         <h1 className='dark:text-white font-bold text-xl'>{name}</h1>
                     </div>
                     <div className="flex items-center space-x-3">
-                        <button onClick={() => handleWishlist(_id)} type="button" title="Like post" className="flex items-center justify-center">
+                        <button type="button" title="Like post" className="flex items-center justify-center">
 
                             {
-                                wishlised ?
-                                    <img className="w-5 h-5" src="https://img.icons8.com/emoji/2x/heart-suit.png" alt="" />
+                                wishlised?.serviceId === _id ?
+                                    <img onClick={() => handleRemoveWishlist(wishlised?.serviceId)} className="w-5 h-5" src="https://img.icons8.com/emoji/2x/heart-suit.png" alt="" />
                                     :
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className=" w-5 h-5 fill-current">
+                                    <svg onClick={() => handleWishlist()} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className=" w-5 h-5 fill-current">
                                         <path d="M453.122,79.012a128,128,0,0,0-181.087.068l-15.511,15.7L241.142,79.114l-.1-.1a128,128,0,0,0-181.02,0l-6.91,6.91a128,128,0,0,0,0,181.019L235.485,449.314l20.595,21.578.491-.492.533.533L276.4,450.574,460.032,266.94a128.147,128.147,0,0,0,0-181.019ZM437.4,244.313,256.571,425.146,75.738,244.313a96,96,0,0,1,0-135.764l6.911-6.91a96,96,0,0,1,135.713-.051l38.093,38.787,38.274-38.736a96,96,0,0,1,135.765,0l6.91,6.909A96.11,96.11,0,0,1,437.4,244.313Z"></path>
                                     </svg>
                             }
